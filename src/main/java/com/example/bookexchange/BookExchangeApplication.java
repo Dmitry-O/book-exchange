2c27b8b8e6c25a4668bdf075da3a4253ec19f415
Dmitry-O <dimasic9420@gmail.com>
2026-06-02 21:21:01 +0200

    chore(startup-code-polish): Polished startup configuration and book photo flow

--- src/main/java/com/example/bookexchange/BookExchangeApplication.java
+++ src/main/java/com/example/bookexchange/BookExchangeApplication.java
@@ -3,9 +3,21 @@ package com.example.bookexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaRepositories(basePackages = {
+        "com.example.bookexchange.auth.repository",
+        "com.example.bookexchange.book.repository",
+        "com.example.bookexchange.common.city.repository",
+        "com.example.bookexchange.common.notification",
+        "com.example.bookexchange.exchange.repository",
+        "com.example.bookexchange.report.repository",
+        "com.example.bookexchange.user.repository"
+})
+@EnableElasticsearchRepositories(basePackages = "com.example.bookexchange.book.search")
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BookExchangeApplication {
